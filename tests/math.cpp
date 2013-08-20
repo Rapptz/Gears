0f1bed0901e31caab72df45b67068af63f6cf65f
Rapptz <rapptz@gmail.com>
2013-08-20 19:43:13 -0400

    Add math.hpp convenience file

--- tests/math.cpp
+++ tests/math.cpp
@@ -20,7 +20,7 @@
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include <catch.hpp>
-#include <gears/math/uintx.hpp>
+#include <gears/math.hpp>
 
 TEST_CASE("Higher Precision Integer", "[uintx]") {
     using namespace gears::literals;
