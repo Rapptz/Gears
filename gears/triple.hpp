e6eebb3e3992c4b986deaacdfc12372c512e11b8
Rapptz <rapptz@gmail.com>
2013-08-24 03:51:28 -0400

    Add triple class

---
+++ gears/triple.hpp
@@ -0,0 +1,202 @@
+// The MIT License (MIT)
+
+// Copyright (c) 2012-2013 Danny Y., Rapptz
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#ifndef GEARS_TRIPLE_HPP
+#define GEARS_TRIPLE_HPP
+
+#include "meta/alias.hpp"
+#include <cstddef>
+
+namespace gears {
+template<typename T, typename U, typename V>
+struct triple {
+    using first_type  = T;
+    using second_type = U;
+    using third_type  = V;
+
+    T first;
+    U second;
+    V third;
+
+    constexpr triple()              = default;
+    constexpr triple(const triple&) = default;
+    constexpr triple(triple&&)      = default;
+
+    constexpr triple(T&& a, U&& b, V&& c) noexcept: first(std::forward<T>(a)), second(std::forward<U>(b)), third(std::forward<V>(c)) {}
+
+    template<class X, class Y, class Z, EnableIf<std::is_convertible<X, T>, std::is_convertible<Y, U>, std::is_convertible<Z, V>>...>
+    constexpr triple(const triple<X, Y, Z>& t) noexcept: first(t.first), second(t.second), third(t.third) {}
+
+    template<class X, class Y, class Z, EnableIf<std::is_convertible<X, T>, std::is_convertible<Y, U>, std::is_convertible<Z, V>>...>
+    constexpr triple(triple<X, Y, Z>&& t) noexcept: first(std::forward<X>(t.first)), second(std::forward<Y>(t.second)), third(std::forward<Z>(t.third)) {}
+
+    void swap(triple& t) noexcept(noexcept(std::swap(first, t.first))   &&
+                                  noexcept(std::swap(second, t.second)) &&
+                                  noexcept(std::swap(third, t.third))) {
+        using std::swap;
+        swap(first, t.first);
+        swap(second, t.second);
+        swap(third, t.third);
+    }
+};
+
+template<typename T, typename U, typename V>
+constexpr bool operator==(const triple<T, U, V>& lhs, const triple<T, U, V>& rhs) {
+    return lhs.first == rhs.first && lhs.second == rhs.second && lhs.third == rhs.third;
+}
+
+template<typename T, typename U, typename V>
+constexpr bool operator<(const triple<T, U, V>& lhs, const triple<T, U, V>& rhs) {
+    return   lhs.first < rhs.first  ? true  :
+             rhs.first < lhs.first  ? false :
+            lhs.second < rhs.second ? true  :
+            rhs.second < lhs.second ? false :
+             lhs.third < rhs.third;
+}
+
+template<typename T, typename U, typename V>
+constexpr bool operator!=(const triple<T, U, V>& lhs, const triple<T, U, V>& rhs) {
+    return !(lhs == rhs);
+}
+
+template<typename T, typename U, typename V>
+constexpr bool operator>(const triple<T, U, V>& lhs, const triple<T, U, V>& rhs) {
+    return (rhs < lhs);
+}
+
+template<typename T, typename U, typename V>
+constexpr bool operator>=(const triple<T, U, V>& lhs, const triple<T, U, V>& rhs) {
+    return !(lhs < rhs);
+}
+
+template<typename T, typename U, typename V>
+constexpr bool operator<=(const triple<T, U, V>& lhs, const triple<T, U, V>& rhs) {
+    return !(rhs < lhs);
+}
+
+template<typename T, typename U, typename V>
+inline void swap(triple<T, U, V>& lhs, triple<T, U, V>& rhs) noexcept(noexcept(lhs.swap(rhs))) {
+    lhs.swap(rhs);
+}
+
+template<typename T, typename U, typename V>
+constexpr triple<Decay<T>, Decay<U>, Decay<V>> make_triple(T&& t, U&& u, V&& v) {
+    return { std::forward<T>(t), std::forward<U>(u), std::forward<V>(v) };
+}
+} // gears
+
+namespace triple_detail {
+template<size_t N> struct triple_get;
+
+template<>
+struct triple_get<0> {
+    template<typename T, typename U, typename V>
+    static constexpr T& get(gears::triple<T, U, V>& t) {
+        return t.first;
+    }
+
+    template<typename T, typename U, typename V>
+    static constexpr const T& const_get(const gears::triple<T, U, V>& t) {
+        return t.first;
+    }
+
+    template<typename T, typename U, typename V>
+    static constexpr T&& move_get(gears::triple<T, U, V>&& t) {
+        return std::forward<T>(t.first);
+    }
+};
+
+template<>
+struct triple_get<1> {
+    template<typename T, typename U, typename V>
+    static constexpr U& get(gears::triple<T, U, V>& t) {
+        return t.second;
+    }
+
+    template<typename T, typename U, typename V>
+    static constexpr const U& const_get(const gears::triple<T, U, V>& t) {
+        return t.second;
+    }
+
+    template<typename T, typename U, typename V>
+    static constexpr U&& move_get(gears::triple<T, U, V>&& t) {
+        return std::forward<U>(t.second);
+    }
+};
+
+template<>
+struct triple_get<2> {
+    template<typename T, typename U, typename V>
+    static constexpr V& get(gears::triple<T, U, V>& t) {
+        return t.third;
+    }
+
+    template<typename T, typename U, typename V>
+    static constexpr const V& const_get(const gears::triple<T, U, V>& t) {
+        return t.third;
+    }
+
+    template<typename T, typename U, typename V>
+    static constexpr V&& move_get(gears::triple<T, U, V>&& t) {
+        return std::forward<V>(t.third);
+    }
+};
+} // triple_detail
+
+namespace std {
+template<typename T> struct tuple_size;
+template<size_t N, typename T> struct tuple_element;
+
+template<typename T, typename U, typename V>
+struct tuple_size<gears::triple<T, U, V>> : gears::Const<size_t, 3> {};
+
+template<typename T, typename U, typename V>
+struct tuple_element<0, gears::triple<T, U, V>> {
+    using type = T;
+};
+
+template<typename T, typename U, typename V>
+struct tuple_element<1, gears::triple<T, U, V>> {
+    using type = U;
+};
+
+template<typename T, typename U, typename V>
+struct tuple_element<2, gears::triple<T, U, V>> {
+    using type = V;
+};
+
+template<size_t N, typename T, typename U, typename V>
+constexpr gears::Type<tuple_element<N, gears::triple<T, U, V>>>& get(gears::triple<T, U, V>& t) noexcept {
+    return triple_detail::triple_get<N>::get(t);
+}
+
+template<size_t N, typename T, typename U, typename V>
+constexpr const gears::Type<tuple_element<N, gears::triple<T, U, V>>>& get(const gears::triple<T, U, V>& t) noexcept {
+    return triple_detail::triple_get<N>::const_get(t);
+}
+
+template<size_t N, typename T, typename U, typename V>
+constexpr gears::Type<tuple_element<N, gears::triple<T, U, V>>>&& get(gears::triple<T, U, V>&& t) noexcept {
+    return triple_detail::triple_get<N>::move_get(t);
+}
+} // std
+
+#endif // GEARS_TRIPLE_HPP
