e84a491fe99488db333a03475d3ab3013c5b006c
Rapptz <Rapptz@gmail.com>
2013-09-05 15:33:46 -0400

    Add Callable and Generator concepts (Issue #9)

---
+++ gears/concepts/function.hpp
@@ -0,0 +1,69 @@
+// The MIT License (MIT)
+
+// Copyright (c) 2012-2013 Danny Y., Rapptz
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#ifndef GEARS_CONCEPTS_FUNCTION_HPP
+#define GEARS_CONCEPTS_FUNCTION_HPP
+
+#include "alias.hpp"
+
+namespace gears {
+namespace function_detail {
+template<typename...>
+struct void_type {
+    using type = void;
+};
+
+template<typename... Args>
+using Void_ = typename void_type<Args...>::type;
+
+template<typename T>
+void sink(T);
+
+template<typename T, typename... Args>
+using FRet = decltype(std::declval<T>()(std::declval<Args>()...));
+
+template<typename T, typename Signature, typename = void>
+struct is_callable : std::false_type {
+    static_assert(std::is_function<Signature>(), "Signature must be a function");
+};
+
+template<typename T, typename R, typename... Args>
+struct is_callable<T, R(Args...), Void_<decltype(sink<R>(std::declval<T>()(std::declval<Args>()...)))>> : std::true_type {};
+
+template<typename T, typename... Args>
+struct is_callable<T, void(Args...), Void_<FRet<T, Args...>>> : std::is_void<FRet<T, Args...>> {};
+
+struct is_generator {
+    template<typename T>
+    static auto test(int) -> decltype(std::declval<T&>()(), std::true_type{}) {}
+    template<typename>
+    static std::false_type test(...);
+};
+} // function_detail
+
+template<typename T, typename Signature>
+struct Callable : function_detail::is_callable<T, Signature> {};
+
+template<typename T>
+struct Generator : TraitOf<function_detail::is_generator, T> {};
+} // gears
+
+#endif // GEARS_CONCEPTS_FUNCTION_HPP
