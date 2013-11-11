589228116752b22cea8c5453684a36781b294c99
Rapptz <rapptz@gmail.com>
2013-11-10 21:05:24 -0500

    Begin working on documenting gears

---
+++ docs/mainpage.hpp
@@ -0,0 +1,18 @@
+///
+/// \mainpage
+/// 
+/// \section welcome Welcome
+/// Welcome to the official documentation for Gears. You can find all the documentation for 
+/// classes and functions in the <a href="./annotated.html">annotated view</a>.
+/// 
+/// \section whatisgears What is Gears?
+/// Gears is a collection of utilities (henceforth now called "modules") that are helpful to C++11 programmers.
+/// These include a large collection of modules ranging from the string module to the functional module.
+/// 
+/// \section compilersupport Compiler Support
+/// Since Gears makes heavy use of C++11 features, only the compilers with the most support for C++11 are supported. At the
+/// time of this writing, this only includes GCC 4.8.1 or higher and Clang 3.4 or higher. Visual Studio 2012 or lower are 
+/// not supported at all, and you might get lucky with some modules on Visual Studio 2013 although not explicitly supported.
+///
+/// <br />
+/// 
