6c9e9591f7cd76760d1a5b79a2c35f32a5a1bd67
Rapptz <rapptz@gmail.com>
2014-02-22 02:21:00 -0500

    Finish documenting function concepts

--- gears/concepts/function.hpp
+++ gears/concepts/function.hpp
@@ -82,14 +82,54 @@ struct is_generator {
 };
 } // detail
 
+/**
+ * @defgroup function_concepts Function concepts submodule
+ * @ingroup concepts
+ * 
+ * @brief Concepts involving functions.
+ * 
+ * @details A collection of concepts that deal with how functions
+ * are used or invoked. Unfortunately, many of the useful function
+ * concepts are not possible without compiler magic, and thus this 
+ * collection is the least exhaustive. 
+ */
+
+/**
+ * @ingroup function_concepts
+ * @brief Checks if a function is callable with a certain signature.
+ * @details A Binary concept that checks if a function can be called with
+ * a certain provided signature.
+ * 
+ * For example, `void f(int)` can be met with `Callable<T, void(int)>`.
+ * 
+ * @tparam T Type to check.
+ * @tparam Signature Signature of the function to check.
+ */
 template<typename T, typename Signature>
 struct Callable : detail::is_callable<T, Signature> {};
 
+/**
+ * @ingroup function_concepts
+ * @brief Checks if a function returns nothing.
+ * @details A Unary concept that checks if a function returns nothing (i.e. `void`).
+ * 
+ * For example. `void f(int, int);` meets the concept of `Generator`.
+ * 
+ * @tparam T Type to check.
+ */
 template<typename T>
 struct Generator : TraitOf<detail::is_generator, T> {};
 
+/**
+ * @ingroup function_concepts
+ * @brief Checks if a type can be called in general.
+ * @details A Unary concept that checks if a function or type can be invoked
+ * through an `operator()`.  
+ * 
+ * @tparam T Type to check.
+ */
 template<typename T>
-struct Function : std::integral_constant<bool, detail::is_function_impl<T>::value> {};
+struct Function : std::integral_constant<bool, detail::is_function_impl<typename std::remove_pointer<T>::type>::value> {};
 } // concepts
 } // gears
 
