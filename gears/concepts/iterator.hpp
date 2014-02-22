47d82f8d25e0c53db4812974fbc60c6c04cf5848
Rapptz <rapptz@gmail.com>
2014-02-22 02:45:52 -0500

    Finish documenting concepts

--- gears/concepts/iterator.hpp
+++ gears/concepts/iterator.hpp
@@ -44,6 +44,35 @@ struct is_random_access {
 };
 } // detail
 
+/**
+ * @defgroup iterator_concepts Iterator concepts submodule
+ * @ingroup concepts
+ * 
+ * @brief Concepts involving iterators.
+ * 
+ * @details A collection of concepts dealing with iterators
+ * used in the standard.
+ * 
+ * Concepts prefixed with Mutable in this collection mean that they are 
+ * OutputIterators simultaneously.
+ */
+
+/**
+ * @ingroup iterator_concepts
+ * @brief Checks if a type is an iterator.
+ * @details A Unary concept to check if a type is an iterator.
+ * Note that this doesn't mean that it inherits from `std::iterator`.
+ * 
+ * In order to meet this concept, the type must meet these concepts:
+ * 
+ * - CopyConstructible
+ * - CopyAssignable
+ * - Destructible
+ * - Dereferenceable
+ * - Incrementable
+ * 
+ * @tparam T Type to check.
+ */
 template<typename T>
 struct Iterator : And<CopyConstructible<T>, 
                       CopyAssignable<T>, 
@@ -51,30 +80,149 @@ struct Iterator : And<CopyConstructible<T>,
                       Dereferenceable<T>,
                       Incrementable<T>> {};
 
+/**
+ * @ingroup iterator_concepts
+ * @brief Checks if a type is an input iterator.
+ * @details A Unary concept that checks if a type is an input iterator.
+ * Note that this doesn't mean it inherits from `std::iterator`.
+ * 
+ * In order to meet this concept, the type must be:
+ * 
+ * - Iterator
+ * - EqualityComparable
+ * 
+ * @tparam T Type to check.
+ */
 template<typename T>
 struct InputIterator : And<Iterator<T>, EqualityComparable<T>> {};
 
+/**
+ * @ingroup iterator_concepts
+ * @brief Checks if a type is an output iterator.
+ * @details A Unary concept that checks if a type is an output iterator.
+ * Note that this doesn't mean it inherits from `std::iterator`.
+ * 
+ * In order to meet this concept, the type must be:
+ * 
+ * - Iterator
+ * - Assignable
+ * 
+ * @tparam T Type to check.
+ */
 template<typename T>
 struct OutputIterator : And<Iterator<T>, Assignable<T>> {};
 
+/**
+ * @ingroup iterator_concepts
+ * @brief Checks if a type is a forward iterator.
+ * @details A Unary concept that checks if a type is a forward iterator.
+ * Note that this doesn't mean it inherits from `std::iterator`.
+ * 
+ * In order to meet this concept, the type must be:
+ * 
+ * - InputIterator
+ * - DefaultConstructible
+ * 
+ * @tparam T Type to check.
+ */
 template<typename T>
 struct ForwardIterator : And<InputIterator<T>, DefaultConstructible<T>> {};
 
+/**
+ * @ingroup iterator_concepts
+ * @brief Checks if a type is a mutable forward iterator.
+ * @details A Unary concept that checks if a type is a mutable forward iterator.
+ * Note that this doesn't mean it inherits from `std::iterator`.
+ * 
+ * In order to meet this concept, the type must be:
+ * 
+ * - ForwardIterator
+ * - Assignable
+ * 
+ * @tparam T Type to check.
+ */
 template<typename T>
 struct MutableForwardIterator : And<ForwardIterator<T>, Assignable<T>> {};
 
+/**
+ * @ingroup iterator_concepts
+ * @brief Checks if a type is a bidirectional iterator.
+ * @details A Unary concept that checks if a type is a bidirectional iterator.
+ * Note that this doesn't mean it inherits from `std::iterator`.
+ * 
+ * In order to meet this concept, the type must be:
+ * 
+ * - ForwardIterator
+ * - Decrementable
+ * 
+ * @tparam T Type to check.
+ */
 template<typename T>
 struct BidirectionalIterator : And<ForwardIterator<T>, Decrementable<T>> {};
 
+/**
+ * @ingroup iterator_concepts
+ * @brief Checks if a type is a mutable bidirectional iterator.
+ * @details A Unary concept that checks if a type is a mutable bidirectional iterator.
+ * Note that this doesn't mean it inherits from `std::iterator`.
+ * 
+ * In order to meet this concept, the type must be:
+ * 
+ * - BidirectionalIterator
+ * - Assignable
+ * 
+ * @tparam T Type to check.
+ */
 template<typename T>
 struct MutableBidirectionalIterator : And<MutableForwardIterator<T>, Decrementable<T>> {};
 
+/**
+ * @ingroup iterator_concepts
+ * @brief Checks if a type is a random access iterator.
+ * @details A Unary concept that checks if a type is a random access iterator.
+ * Note that this doesn't mean it inherits from `std::iterator`.
+ * 
+ * In order to meet this concept, the type must be:
+ * 
+ * - BidirectionalIterator
+ * - Comparable
+ * 
+ * In addition, the following expressions must be valid:
+ * 
+ * @code 
+ * auto& a = (x += n);
+ * auto b = x + n;
+ * auto c = n + x;
+ * auto& d = (x -= n);
+ * auto e = x - n;
+ * auto f = n - x;
+ * auto g = x - y;
+ * auto& h = x[n];
+ * @endcode
+ * 
+ * Where x and y are instances of T. 
+ * 
+ * @tparam T Type to check.
+ */
 template<typename T>
 struct RandomAccessIterator : Or<Pointer<T>,
                                  And<BidirectionalIterator<T>, 
                                      Comparable<T>, 
                                      TraitOf<detail::is_random_access, T>>> {};
 
+/**
+ * @ingroup iterator_concepts
+ * @brief Checks if a type is a mutable random access iterator.
+ * @details A Unary concept that checks if a type is a mutable random access iterator.
+ * Note that this doesn't mean it inherits from `std::iterator`.
+ * 
+ * In order to meet this concept, the type must be:
+ * 
+ * - RandomAccessIterator
+ * - Assignable
+ * 
+ * @tparam T Type to check.
+ */
 template<typename T>
 struct MutableRandomAccessIterator : And<RandomAccessIterator<T>, Assignable<T>> {};
 } // concepts
