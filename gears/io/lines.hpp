9786a1e65726ba085463652646faccfb97b51e03
Rapptz <rapptz@gmail.com>
2014-05-24 03:16:43 -0400

    Finish documenting io module

--- gears/io/lines.hpp
+++ gears/io/lines.hpp
@@ -28,6 +28,16 @@
 
 namespace gears {
 namespace io {
+/**
+ * @ingroup io
+ * @brief Iterator that iterates through stdin lines.
+ * @details An iterator that iterates through lines given by stdin such as
+ * `std::cin`, `std::ifstream`, and `std::istringstream`. This iterator is basically
+ * a wrapper around `std::getline` and `std::istream`. This should not be used directly.
+ *
+ * @tparam CharT Underlying character type of the string.
+ * @tparam Traits Underlying `std::char_traits`-like of the string.
+ */
 template<typename CharT = char, typename Traits = std::char_traits<CharT>>
 struct line_iterator : std::iterator<std::input_iterator_tag, std::basic_string<CharT, Traits>> {
 private:
@@ -68,6 +78,15 @@ public:
     }
 };
 
+/**
+ * @ingroup io
+ * @brief A range object that returns line_iterators.
+ * @details A range object that returns line_iterators. This shouldn't be used
+ * directly and instead should be used with `lines`.
+ *
+ * @tparam CharT Underlying character type.
+ * @tparam Traits Underlying character traits type.
+ */
 template<typename CharT, typename Traits>
 struct line_reader {
 private:
@@ -84,6 +103,15 @@ public:
     }
 };
 
+/**
+ * @ingroup io
+ * @brief Returns a range object to iterate through input lines.
+ * @details Iterates through input lines. Best used with the range-based
+ * for loop.
+ *
+ * @param in `std::istream` derived object to iterate through lines
+ * @return `line_reader` object to iterate through.
+ */
 template<typename CharT, typename Traits>
 inline line_reader<CharT, Traits> lines(std::basic_istream<CharT, Traits>& in) {
     return line_reader<CharT, Traits>(in);
