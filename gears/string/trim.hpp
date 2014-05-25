08939ffd31b371894d5aaa51a8e8ece70af20a39
Rapptz <rapptz@gmail.com>
2014-05-24 22:51:59 -0400

    Finish documenting string module.
    Also made it so trim functions return the original string rather than
    returning an empty string if the predicate is not met.

--- gears/string/trim.hpp
+++ gears/string/trim.hpp
@@ -27,39 +27,124 @@
 
 namespace gears {
 namespace string {
+/**
+ * @ingroup string
+ * @brief Removes the left part of the string until the predicate is met.
+ * @details Removes the left part of the string until the predicate is met.
+ *
+ * Example:
+ * @code
+ * auto str = string::trim_left_if("12984713hello world"_s, [](char c) {
+ *     return std::isdigit(c);
+ * });
+ * // str == "hello world"
+ * @endcode
+ *
+ * If the predicate is never met, returns the original string.
+ *
+ * @param str String to search through.
+ * @param pred Predicate to use.
+ * @return A string with the left part removed.
+ */
 template<typename String, typename UnaryPredicate>
-inline meta::Unqualified<String> trim_left_if(String&& str, UnaryPredicate&& pred) {
-    auto pos = find_first_not_of(std::forward<String>(str), std::forward<UnaryPredicate>(pred));
+inline meta::Unqualified<String> trim_left_if(String str, UnaryPredicate&& pred) {
+    auto pos = find_first_not_of(str, std::forward<UnaryPredicate>(pred));
     if(pos != meta::Unqualified<String>::npos) {
         return { str.substr(pos) };
     }
-    return { };
+    return str;
 }
 
+/**
+ * @ingroup string
+ * @brief Removes the right part of the string until the predicate is met.
+ * @details Removes the right part of the string until the predicate is met.
+ *
+ * Example:
+ * @code
+ * auto str = string::trim_right_if("hello world18324728361"_s, [](char c) {
+ *     return std::isdigit(c);
+ * });
+ * // str == "hello world"
+ * @endcode
+ *
+ * If the predicate is never met, returns the original string.
+ *
+ * @param str String to search through.
+ * @param pred Predicate to use.
+ * @return A string with the right part removed.
+ */
 template<typename String, typename UnaryPredicate>
-inline meta::Unqualified<String> trim_right_if(String&& str, UnaryPredicate&& pred) {
-    auto pos = find_last_not_of(std::forward<String>(str), std::forward<UnaryPredicate>(pred));
+inline meta::Unqualified<String> trim_right_if(String str, UnaryPredicate&& pred) {
+    auto pos = find_last_not_of(str, std::forward<UnaryPredicate>(pred));
     if(pos != meta::Unqualified<String>::npos) {
         return { str.substr(0, pos + 1) };
     }
-    return { };
+    return str;
 }
 
+/**
+ * @ingroup string
+ * @brief Removes both the left and the right part of the string given a predicate.
+ * @details Removes both the left and the right part of the string given a predicate.
+ *
+ * Example:
+ * @code
+ * auto str = string::trim_right_if("123hello world123"_s, [](char c) {
+ *     return std::isdigit(c);
+ * });
+ * // str == "hello world"
+ * @endcode
+ *
+ * @param str The string to search through.
+ * @param pred The predicate to use.
+ * @return A string with the right and left part removed.
+ */
 template<typename String, typename UnaryPredicate>
 inline meta::Unqualified<String> trim_if(String&& str, UnaryPredicate&& pred) {
     return trim_left_if(trim_right_if(std::forward<String>(str), pred), pred);
 }
 
+/**
+ * @ingroup string
+ * @brief Removes the space characters from the left of the string.
+ * @details Removes the space characters from the left of the string as
+ * classified by the locale.
+ *
+ * @param str The string to trim.
+ * @param loc The locale to classify spaces with.
+ * @return A string with the left spaces removed.
+ */
 template<typename String>
 inline meta::Unqualified<String> trim_left(String&& str, const std::locale& loc = std::locale()) {
     return trim_left_if(std::forward<String>(str), is_space(loc));
 }
 
+/**
+ * @ingroup string
+ * @brief Removes the space characters from the right of the string.
+ * @details Removes the space characters from the right of the string as
+ * classified by the locale.
+ *
+ * @param str The string to trim.
+ * @param loc The locale to classify spaces with.
+ * @return A string with the right spaces removed.
+ */
 template<typename String>
 inline meta::Unqualified<String> trim_right(String&& str, const std::locale& loc = std::locale()) {
     return trim_right_if(std::forward<String>(str), is_space(loc));
 }
 
+/**
+ * @ingroup string
+ * @brief Removes the space characters from both the left and right of the string.
+ * @details Removes the space characters from both the left and right of the string as
+ * classified by the locale.
+ *
+ * @param str The string to trim.
+ * @param loc The locale to classify spaces with.
+ * @return A string with both the left and right spaces removed.
+ */
 template<typename String>
 inline meta::Unqualified<String> trim(String&& str, const std::locale& loc = std::locale()) {
     return trim_left(trim_right(std::forward<String>(str), loc), loc);
